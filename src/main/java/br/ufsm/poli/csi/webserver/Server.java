833373967d8daf49f09181fee9eefcc5aca75942
Deborablara <debora.blara19@gmail.com>
2023-05-29 09:54:17 -0300

    add threads

--- src/main/java/br/ufsm/poli/csi/webserver/Server.java
+++ src/main/java/br/ufsm/poli/csi/webserver/Server.java
@@ -21,6 +21,140 @@ public class Server {
     private static List<Poltrona> poltronas;
     private static List<Reserva> reservas;
 
+    private static class RequestHandler extends Thread {
+        private Socket socket;
+
+        public RequestHandler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                InputStream in = socket.getInputStream();
+                OutputStream out = socket.getOutputStream();
+
+                // Lógica de tratamento da requisição
+                byte[] buffer = new byte[1024];
+                int nBytes = in.read(buffer);
+                if (nBytes == -1) {
+                    socket.close();
+                    return;
+                }
+                String str = new String(buffer, 0, nBytes);
+                String[] linhas = str.split("\n");
+                String[] linha1 = linhas[0].split(" ");
+                String recurso = linha1[1];
+
+                if (recurso.equals("/")) {
+                    recurso = "/index.html";
+                } else if (recurso.contains("/compraingresso")) { // formulário
+                    Reserva reserva = extrairParametrosGet("http://localhost:8080" + recurso);
+
+                    // Exibindo a reserva
+                    if (reserva != null) {
+                        String enderecoIP = socket.getInetAddress().getHostAddress();
+                        reserva.setIp(enderecoIP);
+
+                        String log = "Nome do reservante: " + reserva.getNomeReservante() + "\n"
+                                + "ID da Poltrona: " + reserva.getPoltrona().getId() + "\n" +
+                                "Data da Reserva: " + reserva.getDataReserva() + "\n" +
+                                "Endereço de IP: " + reserva.getIp() + "\n\n\n";
+
+                        synchronized (reservas) {
+                            reservas.add(reserva);
+                        }
+
+                        FileOutputStream logs = new FileOutputStream("log.txt", true);
+                        logs.write(log.getBytes(StandardCharsets.UTF_8));
+                        logs.flush();
+                        logs.close();
+                    }
+                    recurso = "/index.html";
+                }
+
+                recurso = recurso.replace('/', File.separatorChar);
+
+                String header = "HTTP/1.1 200 OK\n" +
+                        "Content-Type: " + getContentType(recurso) + "; charset=utf-8\n\n";
+                File f = new File("arquivos_html" + recurso);
+                ByteArrayOutputStream bout = new ByteArrayOutputStream();
+                if (!f.exists()) {
+                    bout.write("404 NOT FOUND\n\n".getBytes(StandardCharsets.UTF_8));
+                } else {
+                    InputStream fileIn = new FileInputStream(f);
+                    bout.write(header.getBytes(StandardCharsets.UTF_8));
+                    // escreve arquivo
+                    nBytes = fileIn.read(buffer);
+                    do {
+                        if (nBytes > 0) {
+                            bout.write(buffer, 0, nBytes);
+                            nBytes = fileIn.read(buffer);
+                        }
+                    } while (nBytes == 1024);
+                    if (nBytes > 0) {
+                        bout.write(buffer, 0, nBytes);
+                    }
+                }
+                if (getContentType(recurso).equals("text/html")) {
+                    String saida = processaVariaveis(bout);
+                    out.write(saida.getBytes(StandardCharsets.UTF_8));
+                } else {
+                    out.write(bout.toByteArray());
+                }
+
+                out.flush();
+                out.close();
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static List<Poltrona> getPoltronas() {
+        if (poltronas == null) {
+            poltronas = new ArrayList<>();
+
+            for (int i = 1; i < 385; i++) {
+                Poltrona poltrona = new Poltrona(i);
+                poltronas.add(poltrona);
+            }
+        }
+
+        return poltronas;
+    }
+
+    private static String processaVariaveis(ByteArrayOutputStream bout) {
+        List<String> poltronasHtml = new ArrayList<>();
+
+        for (Poltrona poltrona : poltronas) {
+            Reserva r = getReservaByPoltronaId(poltrona.getId());
+            poltronasHtml.add(poltrona.getPoltronaHTML(r));
+        }
+
+        String str = new String(bout.toByteArray());
+        String poltronasHtmlString = String.join("", poltronasHtml);
+        str = str.replace("${poltronas}", poltronasHtmlString);
+        return str;
+    }
+
+    private static String getContentType(String nomeRecurso) {
+
+        if (nomeRecurso.toLowerCase().endsWith(".css")) {
+            return "text/css";
+        } else if (nomeRecurso.toLowerCase().endsWith(".jpg")
+                || nomeRecurso.toLowerCase().endsWith(".jpeg")) {
+            return "image/jpeg";
+        } else if (nomeRecurso.toLowerCase().endsWith(".png")) {
+            return "image/png";
+        } else if (nomeRecurso.toLowerCase().contains(".js")) {
+            return "application/javascript";
+        } else {
+            return "text/html";
+        }
+    }
+
     // extrai parametros da url
     public static Reserva extrairParametrosGet(String urlString) {
         Reserva reserva = null;
@@ -96,7 +230,7 @@ public class Server {
         }
 
         Optional<Reserva> optionalReserva = reservas.stream()
-                .filter(reserva -> reserva.getPoltrona().getId() == id)
+                .filter(reserva -> reserva.getPoltrona().getId() == id && reserva.getPoltrona().isReservada())
                 .findFirst();
 
         return optionalReserva.orElse(null);
@@ -104,141 +238,18 @@ public class Server {
 
     public static void main(String[] args) throws IOException {
         poltronas = getPoltronas();
+        reservas = new ArrayList<>();
 
         ServerSocket ss = new ServerSocket(8080);
-        OutputStream logs = new FileOutputStream("log.txt");
         System.out.println("Iniciando server socket...");
 
-        // 1. recebe conexão
         while (true) {
             Socket socket = ss.accept();
             System.out.println("Conexão recebida");
-            InputStream in = socket.getInputStream();
-            OutputStream out = socket.getOutputStream();
 
-            // 2. recebe requisição
-            byte[] buffer = new byte[1024];
-            int nBytes = in.read(buffer);
-            if (nBytes == -1) {
-                socket.close();
-                continue;
-            }
-            String str = new String(buffer, 0, nBytes);
-            String[] linhas = str.split("\n");
-            String[] linha1 = linhas[0].split(" ");
-            String recurso = linha1[1];
-            for (String linha : linhas) {
-                System.out.println(linha);
-            }
-
-            if (recurso.equals("/")) {
-
-                recurso = "/index.html";
-
-            } else if (recurso.contains("/compraingresso")) { // formulário
-
-                Reserva reserva = extrairParametrosGet("http://localhost:8080" + recurso);
-
-                // Exibindo a reserva
-                if (reserva != null) {
-
-                    System.out.println("Nome: " + reserva.getNomeReservante());
-                    System.out.println("ID da Poltrona: " + reserva.getPoltrona().getId());
-                    System.out.println("Data da Reserva: " + reserva.getDataReserva());
-                    System.out.println("Reservada: " + reserva.getPoltrona().isReservada());
-                    String enderecoIP = socket.getInetAddress().getHostAddress();
-                    reserva.setIp(enderecoIP);
-
-                    String log = "Nome do reservante: " + reserva.getNomeReservante() + "\n"
-                            + "ID da Poltrona: " + reserva.getPoltrona().getId() + "\n" +
-                            "Data da Reserva: " + reserva.getDataReserva() + "\n" +
-                            "Endereço de IP: " + reserva.getIp();
-
-                    reservas.add(reserva);
-
-                    logs.write(log.getBytes(StandardCharsets.UTF_8));
-                    logs.flush();
-
-                }
-                recurso = "/index.html";
-            }
-
-            recurso = recurso.replace('/', File.separatorChar);
-
-            String header = "HTTP/1.1 200 OK\n" +
-                    "Content-Type: " + getContentType(recurso) + "; charset=utf-8\n\n";
-            File f = new File("arquivos_html" + recurso);
-            ByteArrayOutputStream bout = new ByteArrayOutputStream();
-            if (!f.exists()) {
-                bout.write("404 NOT FOUND\n\n".getBytes(StandardCharsets.UTF_8));
-            } else {
-                InputStream fileIn = new FileInputStream(f);
-                bout.write(header.getBytes(StandardCharsets.UTF_8));
-                // escreve arquivo
-                nBytes = fileIn.read(buffer);
-                do {
-                    if (nBytes > 0) {
-                        bout.write(buffer, 0, nBytes);
-                        nBytes = fileIn.read(buffer);
-                    }
-                } while (nBytes == 1024);
-                if (nBytes > 0) {
-                    bout.write(buffer, 0, nBytes);
-                }
-            }
-            if (getContentType(recurso) == "text/html") {
-                String saida = processaVariaveis(bout);
-                out.write(saida.getBytes(StandardCharsets.UTF_8));
-            } else {
-                out.write(bout.toByteArray());
-            }
-
-            out.flush();
-            out.close();
-            socket.close();
-        }
-    }
-
-    private static List<Poltrona> getPoltronas() {
-        if (poltronas == null) {
-            poltronas = new ArrayList<>();
-
-            for (int i = 1; i < 385; i++) {
-                Poltrona poltrona = new Poltrona(i);
-                poltronas.add(poltrona);
-            }
-        }
-
-        return poltronas;
-    }
-
-    private static String processaVariaveis(ByteArrayOutputStream bout) {
-        List<String> poltronasHtml = new ArrayList<>();
-
-        for (Poltrona poltrona : poltronas) {
-            Reserva r = getReservaByPoltronaId(poltrona.getId());
-            poltronasHtml.add(poltrona.getPoltronaHTML(r));
-        }
-
-        String str = new String(bout.toByteArray());
-        String poltronasHtmlString = String.join("", poltronasHtml);
-        str = str.replace("${poltronas}", poltronasHtmlString);
-        return str;
-    }
-
-    private static String getContentType(String nomeRecurso) {
-
-        if (nomeRecurso.toLowerCase().endsWith(".css")) {
-            return "text/css";
-        } else if (nomeRecurso.toLowerCase().endsWith(".jpg")
-                || nomeRecurso.toLowerCase().endsWith(".jpeg")) {
-            return "image/jpeg";
-        } else if (nomeRecurso.toLowerCase().endsWith(".png")) {
-            return "image/png";
-        } else if (nomeRecurso.toLowerCase().contains(".js")) {
-            return "application/javascript";
-        } else {
-            return "text/html";
+            // Cria uma nova instância de RequestHandler e inicia a thread
+            RequestHandler requestHandler = new RequestHandler(socket);
+            requestHandler.start();
         }
     }
 
